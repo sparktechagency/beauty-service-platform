ab9f3509e7726ab098d70bd236d0ba7b3dbb9961
mdsharifulislam-r <masom3456@gmail.com>
2025-06-02 10:13:15 +0600

    update clenup function

--- src/cleanup/cleanUp.ts
+++ src/cleanup/cleanUp.ts
@@ -3,19 +3,47 @@ import { Subscription } from "../app/modules/subscription/subscription.model";
 import { User } from "../app/modules/user/user.model";
 import { UserTakeService } from "../app/modules/usertakeservice/usertakeservice.model";
 import { UserTakeServiceServices } from "../app/modules/usertakeservice/usertakeservice.service";
+import { Plan } from "../app/modules/plan/plan.model";
 
 const expireSubscriptions = async ()=>{
     const subscriptions = await Subscription.find({
         status: "active",
         currentPeriodEnd: { $lte: new Date() },
     }).lean()
+    
+    
     subscriptions.forEach(async (subscription) => {
-        await Subscription.findByIdAndUpdate(subscription._id, {
+        const subscribePlan:any = await Subscription.findByIdAndUpdate(subscription._id, {
             status: "expired",
-        });
+        },{new:true}).populate('package').lean().exec()
+        
+
+        const freeSubscription = await Plan.find({
+            for:subscribePlan?.package?.for,
+        }).sort({price:1}).lean().exec()
+        
+        
+        
+        const firstFreePlan = freeSubscription[0]
+
+           const sub = await Subscription.create({
+                user:subscription.user,
+                package:firstFreePlan._id,
+                status:"active",
+                currentPeriodStart:new Date(),
+                currentPeriodEnd:new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0),
+                price:firstFreePlan.price,
+                customerId:crypto.randomUUID(),
+                subscriptionId:crypto.randomUUID(),
+                trxId:crypto.randomUUID()
+            })
+
+        
+            
+        
 
         await User.findByIdAndUpdate(subscription.user, {
-            subscription: null,
+            subscription: sub._id,
         });
     });
 
@@ -26,7 +54,7 @@ const expireSubscriptions = async ()=>{
 
 
 export const cleanUp =()=>{
-   cron.schedule("0 0 * * *", async()=>{
+   cron.schedule("0j 0 * * *", async()=>{
         await expireSubscriptions();
         console.log("Expired subscriptions");
    },{
