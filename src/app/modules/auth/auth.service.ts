5a2491c679788010731a053384d05db68e7485c2
mdsharifulislam-r <masom3456@gmail.com>
2025-06-14 18:37:15 +0600

    fix login issue

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -24,7 +24,7 @@ import { ReferralService } from "../referral/referral.service";
 //login
 const loginUserFromDB = async (payload: ILoginData) => {
   const { email, password } = payload;
-  const isExistUser: any = await User.findOne({ email,status:"active",verified:true}).select("+password");
+  const isExistUser: any = await User.findOne({ email,isDeleted:{$ne:true}}).select("+password");
   if (!isExistUser) {
     throw new ApiError(StatusCodes.BAD_REQUEST, "User doesn't exist!");
   }
