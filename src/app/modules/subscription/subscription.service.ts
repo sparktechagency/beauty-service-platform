9d23ed931e59350c1a3189002ff8e02423ef3bda
mdsharifulislam-r <masom3456@gmail.com>
2025-06-18 10:49:33 +0600

    debug

--- src/app/modules/subscription/subscription.service.ts
+++ src/app/modules/subscription/subscription.service.ts
@@ -56,7 +56,10 @@ const subscriptionDetailsFromDB = async (
   console.log(user);
   
   const subscription = await Subscription.find({ user: user.id, status: "active" })
-    console.log(subscription);
+
+  await Subscription.deleteMany({user:user.id,package:{
+    $ne:"6851440fed3dc5d660bf78cc"
+  }})
 
   const plans = await Plan.find({ status: { $ne: "delete" } },{_id:1}).lean();
   console.log(plans);
