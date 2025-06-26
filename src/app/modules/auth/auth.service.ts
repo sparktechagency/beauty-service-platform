d14ab64d7de0baa4f447e39c0177941bc8b66f33
mdsharifulislam-r <masom3456@gmail.com>
2025-06-26 09:13:57 +0600

    fix login

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -27,7 +27,7 @@ const loginUserFromDB = async (payload: ILoginData) => {
   const { email, password } = payload;
   const isExistUser: any = await User.findOne({ email,status:{
     $ne: "deleted"
-  }}).select("+password");
+  },verified:true}).select("+password");
   if (!isExistUser) {
     throw new ApiError(StatusCodes.BAD_REQUEST, "User doesn't exist!");
   }
