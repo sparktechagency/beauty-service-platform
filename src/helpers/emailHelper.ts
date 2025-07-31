16ba571e7c78adf96cb86309b2b30e7acb8056af
mdsharifulislam-r <masom3456@gmail.com>
2025-07-31 17:27:35 +0600

    fix the problem

--- src/helpers/emailHelper.ts
+++ src/helpers/emailHelper.ts
@@ -16,7 +16,7 @@ const transporter = nodemailer.createTransport({
 const sendEmail = async (values: ISendEmail) => {
     try {
         const info = await transporter.sendMail({
-            from: `"Ooh Ah" ${config.email.from}`,
+            from: `"Ooh Ah" <no-reply@oohahplatform.com>`,
             to: values.to,
             subject: values.subject,
             html: values.html,
